2d9f79fe2eceb5bb99382e148cada30b3d602ede
Aadarsh Mishra <aadarsh9600@gmail.com>
2022-04-13 22:49:06 +0530

    added calc component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalcComponent } from './calc/calc.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+    path: 'calc',
+    component:CalcComponent
+},
+{
+  path:'**',
+  component:CalcComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
